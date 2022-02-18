0eb8019f0f63c4bdee8aa4efb535403df60a6446
mcbethr <51751681+mcbethr@users.noreply.github.com>
2022-02-18 06:26:21 -0500

    Initial commit

---
+++ BoxingAndUnboxingVideo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+
+		int Fish = 0;
+
+		object BoxedFish = Fish;
+
+		object Chicken = new object();
+
+		object[] Truck = new object[] { Chicken, BoxedFish, Chicken };
+
+		Fish = Convert.ToInt32(BoxedFish);
+
+		/*
+		
+		{
+		  "BoxedFish": "0",
+		  "Chicken": "Chicken object"
+		}
+		
+		*/
+
+	}
+}
